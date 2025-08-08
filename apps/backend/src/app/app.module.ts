9608d69e36282f2a3fef436d5aee516965f2772d
elleryrain <3ll3ry.r41n@gmail.com>
2025-08-08 14:24:08 +0300

    backend -> update auth module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from '../db/db.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [DatabaseModule, ConfigModule, AuthModule],
