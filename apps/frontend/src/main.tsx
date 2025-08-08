9049f5b058f871d7c54c173c782eba7da93fb12b
Artem <kamaltema@yandex.com>
2025-08-08 18:25:35 +0300

    minor

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { ReactNode, StrictMode } from 'react';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './components/router/Router';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <StrictMode>
-    <RouterProvider router={router} future={{ v7_startTransition: true, }}/>
-  </StrictMode>
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  </StrictMode> as ReactNode
 );
